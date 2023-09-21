181662b53bc93e7ff03dfb2de0fd7ac2c80d1c9e
Maximilien Pelletier <maximilienpelletier@gmail.com>
2023-09-21 15:54:56 +0200

    Hello Strades

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
   </div>
+  <h1 class="hello">Hello World!</h1>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
