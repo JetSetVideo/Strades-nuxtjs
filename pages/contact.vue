d7e8d9bb08665ae68253e937d2121687da50f1a5
Maximilien Pelletier <maximilienpelletier@gmail.com>
2023-10-04 23:10:54 +0200

    contact update

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,39 +1,30 @@
-<script setup>
-  export default {
-    data() {
-      return {
-        message: '',
-      };
-    },
-    methods: {
-      sendEmail() {
-        // You can implement the logic to send an email here
-        // Example: Send an email using your preferred email service/API
-        const email = 'maximilien.pelletier@Strades.app';
-        const subject = 'Contact from Strades App';
-        const body = this.message;
-        const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
-        window.location.href = mailtoLink;
-      },
-    },
-  };
-</script>
-
 <template>
     <div class="contact-page">
       <div class="background"></div>
       <section class="content">
         <h1>Contact Us</h1>
         <p>If you have any questions or feedback, feel free to reach out to us. We'd love to hear from you!</p>
-        
+  
         <!-- Contact Form -->
-        <form @submit.prevent="sendEmail">
-          <label for="message">Message:</label>
-          <textarea id="message" v-model="message" required></textarea>
-          
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" v-model="name" required />
+          </div>
+  
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required />
+          </div>
+  
+          <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea id="message" v-model="message" required></textarea>
+          </div>
+  
           <button type="submit">Send Message</button>
         </form>
-        
+  
         <!-- Social Links -->
         <div class="social-links">
           <a href="https://twitter.com/StradesApp" target="_blank" rel="noopener noreferrer">
@@ -46,105 +37,45 @@
         </div>
       </section>
     </div>
-</template>
+  </template>
   
-<style scoped>
-  .contact-page {
-    position: relative;
-    width: 100%;
-    height: 100vh;
-    overflow: hidden;
-  }
+  <script>
+  export default {
+    data() {
+      return {
+        name: '',
+        email: '',
+        message: '',
+      };
+    },
+    methods: {
+      submitForm() {
+        // Here, you can implement the logic to send the message.
+        // You can use a library or service like Axios to make an API request to your server.
   
-  .background {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(90deg, #333 10%, #444 50%, #333 90%);
-    background-size: 200% 100%;
-    animation: moveWaves 20s linear infinite;
-  }
+        // Example:
+        const formData = {
+          name: this.name,
+          email: this.email,
+          message: this.message,
+        };
   
-  .content {
-    position: relative;
-    z-index: 1;
-    text-align: center;
-    color: #fff;
-    padding: 20px;
-  }
+        // Make an API request to your server or email service
+        // axios.post('/api/send-email', formData)
+        //   .then(response => {
+        //     // Handle success
+        //     console.log('Message sent successfully', response.data);
+        //   })
+        //   .catch(error => {
+        //     // Handle error
+        //     console.error('Error sending message', error);
+        //   });
+      },
+    },
+  };
+  </script>
   
-  h1 {
-    font-size: 36px;
-  }
-  
-  p {
-    font-size: 18px;
-    margin: 20px 0;
-  }
-  
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  label {
-    font-size: 18px;
-    margin-top: 10px;
-  }
-  
-  textarea {
-    width: 100%;
-    max-width: 400px;
-    height: 150px;
-    font-size: 16px;
-    padding: 10px;
-    margin: 10px 0;
-    border: 1px solid #fff;
-    border-radius: 5px;
-  }
-  
-  button {
-    font-size: 18px;
-    padding: 10px 20px;
-    background-color: #fff;
-    color: #333;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  
-  button:hover {
-    background-color: #444;
-    color: #fff;
-  }
-  
-  .social-links {
-    margin-top: 30px;
-    display: flex;
-    justify-content: center;
-  }
-  
-  .social-links a {
-    font-size: 20px;
-    margin: 0 10px;
-    color: #fff;
-    text-decoration: none;
-  }
-  
-  .social-links i {
-    margin-right: 5px;
-    font-size: 24px;
-  }
-  
-  @keyframes moveWaves {
-    0% {
-      background-position: 100% 0;
-    }
-    100% {
-      background-position: -100% 0;
-    }
-  }
-</style>
+  <style scoped>
+  /* Your CSS styles for the contact page go here */
+  </style>
   
