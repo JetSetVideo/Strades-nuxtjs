97c87268acb6c8b378e697c9c550a313005f8c2f
Maximilien Pelletier <maximilienpelletier@gmail.com>
2023-10-05 22:29:33 +0200

    ButtonComponent solved

--- components/ButtonComponent.vue
+++ components/ButtonComponent.vue
@@ -1,30 +1,30 @@
 <template>
-    <button class="button">
-      {{ text }}
-    </button>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ButtonComponent',
-    props: {
-      text: String
-    }
-  };
-  </script>
-  
-  <style lang="scss">
-  .button {
-    width: 100px;
-    height: 50px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2), inset 0 1px 0 rgba(255, 255, 255, 0.5);
-    transition: 0.2s all ease-in-out;
-  }
-  
-  .button:hover {
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2), inset 0 2px 0 rgba(255, 255, 255, 0.5);
-  }
-  </style>
+  <button class="button">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.button {
+  width: 100px;
+  height: 50px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2), inset 0 1px 0 rgba(255, 255, 255, 0.5);
+  transition: 0.2s all ease-in-out;
+}
+
+.button:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2), inset 0 2px 0 rgba(255, 255, 255, 0.5);
+}
+</style>
